d6101c2528be4588010cc8b318749b31678ecc99
Hooty <cherepov_kir@mail.ru>
2024-08-18 16:01:00 +0300

    Did the final work with the disk

--- script.py
+++ script.py
@@ -1,15 +1,18 @@
 import os
 
 ## Ping test
-print('Test Internet Connection')
+print('#Test Internet Connection')
 os.system('ping nhentai.com')
 
 ## Time synchronization
+print('\n#Time synchronization')
 os.system('timedatectl set-ntp true')
 os.system('timedatectl status')
 
 ## Disk partition
+print('\n#Disk partition')
 disk_path = input('Input path to main disk: ')
+part_symbol = 'p' if 'nvme' in disk_path else ''
 
 # Display instructions
 print("Disk Partition Instructions:")
@@ -29,15 +32,9 @@ print('4. "w" for write changes')
 os.system(f'fdisk {disk_path}')
 
 ## Disk encryption
-if 'nvme' in disk_path:
-    os.system(f'cryptsetup luksFormat {disk_path}p3')
-    os.system(f'cryptsetup open {disk_path}p3 luks')
-else:
-    os.system(f'cryptsetup luksFormat {disk_path}3')
-    os.system(f'cryptsetup open {disk_path}3 luks')
-
-# Check the partitions
-os.system('ls /dev/mapper/*')
+print('\n#Disk encryption')
+os.system(f'cryptsetup luksFormat {disk_path}{part_symbol}3')
+os.system(f'cryptsetup open {disk_path}{part_symbol}3 luks')
 
 # Create logical partitions inside the encrypted partition
 os.system('pvcreate /dev/mapper/luks')
@@ -46,5 +43,20 @@ os.system('vgcreate main /dev/mapper/luks')
 # Put 100% of the encrypted partition into the root logical partition
 os.system('lvcreate -l 100%FREE main -n root')
 
-# View all logical partitions
-os.system('lvs')
+## Partition preparation and mounting
+print('\n#Partition preparation and mounting')
+
+# Format boot partition to Fat32
+os.system(f'mkfs.fat -F32 {disk_path}{part_symbol}1')
+
+# Initialize and activate swap partition
+os.system(f'mkswap {disk_path}{part_symbol}2')
+os.system(f'swapon {disk_path}{part_symbol}2')
+
+# Format the partition to ext4
+os.system('mkfs.ext4 /dev/mapper/main-root')
+
+# Mount the partitions for installing the system
+os.system('mount /dev/mapper/main-root /mnt')
+os.system('mkdir /mnt/boot')
+os.system(f'mount {disk_path}{part_symbol}1 /mnt/boot')
