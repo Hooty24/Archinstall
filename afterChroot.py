744ecb49a1366e6da8cccefc25d941cfb1b3731b
Hooty <cherepov_kir@mail.ru>
2024-08-19 12:40:33 +0300

    Improve pacman performance

--- afterChroot.py
+++ afterChroot.py
@@ -1,5 +1,7 @@
 import os
 
+os.system = print
+
 optional_programs = ['linux-zen-headers', 'base-devel', 'ark', 'bluez-utils', 'btop', 'code',
                      'dolphin', 'elisa', 'fastfetch', 'firefox', 'fish', 'fuse2', 'ffmpeg', 'git', 'github-cli',
                      'gparted',
@@ -11,33 +13,40 @@ optional_programs = ['linux-zen-headers', 'base-devel', 'ark', 'bluez-utils', 'b
 yay = ['yay', 'code-marketplace', 'waydroid', 'onlyoffice-bin']
 
 
-# Generate locales
 def replace_line_in_file(file_path, old_line, new_line):
     with open(file_path, 'r') as f:
         lines = f.readlines()
 
     for i, line in enumerate(lines):
         if line.strip() == old_line.strip():
-            print([lines[i]])
-            lines[i] = new_line
+            lines[i] = new_line + '\n'
 
     with open(file_path, 'w') as f:
         f.writelines(lines)
 
 
+# Improve pacman performance
+print('\n#Improving pacman performance')
+replace_line_in_file('/etc/pacman.conf', '#Color', 'Color')
+replace_line_in_file('/etc/pacman.conf', '#ParallelDownloads = 5', 'ParallelDownloads = 15')
+replace_line_in_file('/etc/pacman.conf', '#[multilib]', '[multilib]')
+replace_line_in_file('/etc/pacman.conf', '#Include = /etc/pacman.d/mirrorlist', 'Include = /etc/pacman.d/mirrorlist')
+
+# Generate locales
 print('\n#Generate locales')
-replace_line_in_file('/etc/locale.gen', '#ru_RU.UTF-8 UTF-8', 'ru_RU.UTF-8 UTF-8\n')
-replace_line_in_file('/etc/locale.gen', '#en_US.UTF-8 UTF-8', 'en_US.UTF-8 UTF-8\n')
+replace_line_in_file('/etc/locale.gen', '#ru_RU.UTF-8 UTF-8', 'ru_RU.UTF-8 UTF-8')
+replace_line_in_file('/etc/locale.gen', '#en_US.UTF-8 UTF-8', 'en_US.UTF-8 UTF-8')
 os.system('locale-gen')
 
 # Set the time
-print('\n#Set the time')
+print('\n#Setting the time')
 os.system('ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime')
 os.system('hwclock --systohc')
 
 # Specify the hostname
-print('\n#Specify the hostname')
+print('\n#Specifying the hostname')
 os.system('echo "arch" > /etc/hostname')
 
 # Specify the password for the root user
+print('\n#Specifying the password for the root user')
 os.system('passwd')
